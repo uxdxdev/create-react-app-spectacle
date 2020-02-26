14e1b349a4b31eb47e1f4ae8b6e9890559fcc47c
DMorton <dmorton@ie.ibm.com>
2020-02-26 11:06:56 +0000

    Updates

---
+++ precache-manifest.92ba1e56428278d39e0f6f5548bfc615.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a36a4cbbf7e369b249b98bd774b501",
+    "url": "/create-react-app-spectacle/index.html"
+  },
+  {
+    "revision": "db3e179115d0db65b2ae",
+    "url": "/create-react-app-spectacle/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "497258d2a3cabb663e1f",
+    "url": "/create-react-app-spectacle/static/js/0.575bd89c.chunk.js"
+  },
+  {
+    "revision": "38b887f47ae46080fa6a63b0401e5fa2",
+    "url": "/create-react-app-spectacle/static/js/0.575bd89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88572dbe2068231021cc",
+    "url": "/create-react-app-spectacle/static/js/1.6c04db4a.chunk.js"
+  },
+  {
+    "revision": "a3ada795076054407e15",
+    "url": "/create-react-app-spectacle/static/js/10.1ef4d690.chunk.js"
+  },
+  {
+    "revision": "2e1f0cecd03ffb05b280",
+    "url": "/create-react-app-spectacle/static/js/11.862e8ca7.chunk.js"
+  },
+  {
+    "revision": "d62fb7185cc7c4832255",
+    "url": "/create-react-app-spectacle/static/js/12.5c4685b9.chunk.js"
+  },
+  {
+    "revision": "7b45589a84f7cc18669d",
+    "url": "/create-react-app-spectacle/static/js/13.2e921708.chunk.js"
+  },
+  {
+    "revision": "cad8e508c01cc4c407c6",
+    "url": "/create-react-app-spectacle/static/js/14.2ac96fcc.chunk.js"
+  },
+  {
+    "revision": "6e39759f573f18224586",
+    "url": "/create-react-app-spectacle/static/js/15.94a33c77.chunk.js"
+  },
+  {
+    "revision": "c65e17d98b18f1dc84cd",
+    "url": "/create-react-app-spectacle/static/js/16.a0290d1f.chunk.js"
+  },
+  {
+    "revision": "6da9e5f659daa9f92d1a",
+    "url": "/create-react-app-spectacle/static/js/17.a5926a57.chunk.js"
+  },
+  {
+    "revision": "469f8da737de71292781",
+    "url": "/create-react-app-spectacle/static/js/18.2a04574b.chunk.js"
+  },
+  {
+    "revision": "8d57d22785108aacf5cc",
+    "url": "/create-react-app-spectacle/static/js/19.f7e2673d.chunk.js"
+  },
+  {
+    "revision": "34ff5dde159943c9f7c9",
+    "url": "/create-react-app-spectacle/static/js/2.d34fc90b.chunk.js"
+  },
+  {
+    "revision": "fb98fe8462226034e2af",
+    "url": "/create-react-app-spectacle/static/js/5.36419dfb.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/create-react-app-spectacle/static/js/5.36419dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f355802ede82ca43da",
+    "url": "/create-react-app-spectacle/static/js/6.4976d9fd.chunk.js"
+  },
+  {
+    "revision": "dd0a66942bf41b4e08b6ad12ab7600de",
+    "url": "/create-react-app-spectacle/static/js/6.4976d9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91f4fc3209a2ed824a7",
+    "url": "/create-react-app-spectacle/static/js/7.f76498a6.chunk.js"
+  },
+  {
+    "revision": "e708fb03dbb23543ef22",
+    "url": "/create-react-app-spectacle/static/js/8.0c8e2308.chunk.js"
+  },
+  {
+    "revision": "5f6baf6a1b98e26aece4",
+    "url": "/create-react-app-spectacle/static/js/9.1641dec3.chunk.js"
+  },
+  {
+    "revision": "db3e179115d0db65b2ae",
+    "url": "/create-react-app-spectacle/static/js/main.6ecfb18e.chunk.js"
+  },
+  {
+    "revision": "dde604801b1c9edecbb8",
+    "url": "/create-react-app-spectacle/static/js/runtime-main.b37f7002.js"
+  }
+]);
